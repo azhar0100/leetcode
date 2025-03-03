3ca3d00bf68ae0e3284a9a02fe7bbddf8921e337
azhar0100 <azhartahir0100@gmail.com>
2025-03-04 01:13:50 +0500

    Added code

--- rust/189.rotate-array.rs
+++ rust/189.rotate-array.rs
@@ -5,9 +5,27 @@
  */
 
 // @lc code=start
+
+
+pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+    let len = nums.len();
+    let effective_k = (k % (len as i32)) as usize;
+    let mut k_sized_vec = Vec::with_capacity(effective_k);
+    for o in (0..len).rev(){
+        let n = o+effective_k;
+        match n < len{
+            true => nums[n] = nums[o],
+            false => k_sized_vec.push(nums[o]),
+        }
+    }
+    for (i,val) in k_sized_vec.into_iter().rev().enumerate(){
+        nums[i] = val
+    }
+}
+
 impl Solution {
     pub fn rotate(nums: &mut Vec<i32>, k: i32) {
-        
+        rotate(nums, k)        
     }
 }
 // @lc code=end
--- rust/leetcode-rust/src/problems/p0189_rotate_array.rs
+++ rust/leetcode-rust/src/problems/p0189_rotate_array.rs
@@ -1,23 +1,18 @@
-fn rotated_index(k: i32, n: usize, x: usize) -> usize {
-    (((x as i32) + k) % (n as i32)) as usize
-}
+use std::collections::HashMap;
+
 
 pub fn rotate(nums: &mut Vec<i32>, k: i32) {
-    let n = nums.len();
-    let old_positions = 0..n;
-    let new_positions = (0..n).map(|x| rotated_index(k, n, x));
-    match k.cmp(&0) {
-        std::cmp::Ordering::Less => {
-            old_positions
-                .zip(new_positions)
-                .for_each(|(o, n)| (nums[o], nums[n]) = (nums[n], nums[o]));
+    let len = nums.len();
+    let effective_k = (k % (len as i32)) as usize;
+    let mut k_sized_vec = Vec::with_capacity(effective_k);
+    for o in (0..len).rev(){
+        let n = o+effective_k;
+        match n < len{
+            true => nums[n] = nums[o],
+            false => k_sized_vec.push(nums[o]),
         }
-        std::cmp::Ordering::Equal => (),
-        std::cmp::Ordering::Greater => {
-            old_positions
-                .zip(new_positions)
-                .rev()
-                .for_each(|(o, n)| (nums[o], nums[n]) = (nums[n], nums[o]));
-        }
+    }
+    for (i,val) in k_sized_vec.into_iter().rev().enumerate(){
+        nums[i] = val
     }
 }
