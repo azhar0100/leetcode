2a0c37c6bae836e524ab0362dc936523ba5e9b02
azhar0100 <azhartahir0100@gmail.com>
2025-03-04 08:49:41 +0500

    Added code

---
+++ rust/274.h-index.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=274 lang=rust
+ *
+ * [274] H-Index
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        
+    }
+}
+// @lc code=end
+
--- rust/leetcode-rust/src/problems/mod.rs
+++ rust/leetcode-rust/src/problems/mod.rs
@@ -10,3 +10,4 @@ pub mod p0004_median_of_two_sorted_arrays;
 pub mod p0080_remove_duplicates_from_sorted_array_2;
 pub mod p0189_rotate_array;
 pub mod p0238_product_of_array_except_self;
+pub mod p0274_h_index;
