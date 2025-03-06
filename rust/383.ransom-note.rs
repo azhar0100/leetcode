db490f41a804476090ed25dec214f4fcb304d338
azhar0100 <azhartahir0100@gmail.com>
2025-03-06 09:39:19 +0500

    Added code

--- rust/383.ransom-note.rs
+++ rust/383.ransom-note.rs
@@ -5,10 +5,27 @@
  */
 
 // @lc code=start
+use std::collections::HashMap;
+pub fn string_dictionary(s: &str) -> HashMap<char, usize> {
+    s.chars().fold(HashMap::new(), |mut acc, c| {
+        acc.entry(c).and_modify(|x| *x += 1).or_insert(1);
+        acc
+    })
+}
 impl Solution {
-    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
-        
-    }
+
+
+pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+    let ransom_note_dict = string_dictionary(&ransom_note);
+    let magazine_dict = string_dictionary(&magazine);
+    ransom_note_dict.iter().all(|(note_k, note_v)| {
+        let magazine_v = magazine_dict
+            .get(&note_k)
+            .unwrap_or(&0);
+        note_v <= magazine_v
+    })
+}
+
 }
 // @lc code=end
 
--- rust/leetcode-rust/src/problems/p0383_ransom_note.rs
+++ rust/leetcode-rust/src/problems/p0383_ransom_note.rs
@@ -1,9 +1,18 @@
-pub fn string_dictionary(s:&str) -> Hashmap<char,usize>{
-    s.chars().fold(Hash::new(),|mut acc, c| {
-
+use std::collections::HashMap;
+pub fn string_dictionary(s: &str) -> HashMap<char, usize> {
+    s.chars().fold(HashMap::new(), |mut acc, c| {
+        acc.entry(c).and_modify(|x| *x += 1).or_insert(1);
+        acc
     })
 }
 
 pub fn can_construct(ransom_note: String, magazine: String) -> bool {
-       todo!()
+    let ransom_note_dict = string_dictionary(&ransom_note);
+    let magazine_dict = string_dictionary(&magazine);
+    ransom_note_dict.iter().all(|(note_k, note_v)| {
+        let magazine_v = magazine_dict
+            .get(&note_k)
+            .unwrap_or(&0);
+        note_v <= magazine_v
+    })
 }
