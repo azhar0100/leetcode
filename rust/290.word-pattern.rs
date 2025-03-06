159d920e383504ce44224725b5195f629fe1b474
azhar0100 <azhartahir0100@gmail.com>
2025-03-06 11:11:56 +0500

    Added word pattern

---
+++ rust/290.word-pattern.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=290 lang=rust
+ *
+ * [290] Word Pattern
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn word_pattern(pattern: String, s: String) -> bool {
+        
+    }
+}
+// @lc code=end
+
