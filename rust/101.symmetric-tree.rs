5ef0a8fb374b74be35db87fae30f39e1df254e54
azhar0100 <azhartahir0100@gmail.com>
2025-03-12 13:39:04 +0500

    Added code

--- rust/101.symmetric-tree.rs
+++ rust/101.symmetric-tree.rs
@@ -25,9 +25,40 @@
 // }
 use std::rc::Rc;
 use std::cell::RefCell;
+
+pub fn is_same_tree_mirrored(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match (p, q) {
+        (None, None) => true,
+        (Some(p), Some(q)) => {
+            let p_val = &p.borrow().val.clone();
+            let q_val = &q.borrow().val.clone();
+            p_val == q_val
+                && is_same_tree_mirrored(p.borrow().left.clone(), q.borrow().right.clone())
+                && is_same_tree_mirrored(p.borrow().right.clone(), q.borrow().left.clone())
+        }
+        _ => false,
+    }
+}
+
+
+pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match root {
+        Some(root) => {
+            let root_node = root.borrow();
+            match (&root_node.left, &root_node.right) {
+                (None, None) => true,
+                (None, Some(_)) => false,
+                (Some(_), None) => false,
+                (Some(left), Some(right)) => is_same_tree_mirrored(Some(left.clone()), Some(right.clone())),
+            }
+        }
+        None => true,
+    }
+}
+
 impl Solution {
     pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-        
+        is_symmetric(root)
     }
 }
 // @lc code=end
--- rust/leetcode-rust/src/problems/p101_symmetric_tree.rs
+++ rust/leetcode-rust/src/problems/p101_symmetric_tree.rs
@@ -4,6 +4,21 @@ use crate::util::treenode_leetcode::TreeNode;
 
 use super::p100_same_tree::is_same_tree;
 
+pub fn is_same_tree_mirrored(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match (p, q) {
+        (None, None) => true,
+        (Some(p), Some(q)) => {
+            let p_val = &p.borrow().val.clone();
+            let q_val = &q.borrow().val.clone();
+            p_val == q_val
+                && is_same_tree(p.borrow().left.clone(), q.borrow().right.clone())
+                && is_same_tree(p.borrow().right.clone(), q.borrow().left.clone())
+        }
+        _ => false,
+    }
+}
+
+
 pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
     match root {
         Some(root) => {
@@ -12,7 +27,7 @@ pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
                 (None, None) => true,
                 (None, Some(_)) => false,
                 (Some(_), None) => false,
-                (Some(left), Some(right)) => is_same_tree(Some(left.clone()), Some(right.clone())),
+                (Some(left), Some(right)) => is_same_tree_mirrored(Some(left.clone()), Some(right.clone())),
             }
         }
         None => true,
