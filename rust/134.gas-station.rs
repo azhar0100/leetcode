0a334c9f2ffc01287ec5d71a9989bb4cf811df76
azhar0100 <azhartahir0100@gmail.com>
2025-03-19 00:13:00 +0500

    Added code

--- rust/134.gas-station.rs
+++ rust/134.gas-station.rs
@@ -5,35 +5,50 @@
  */
 
 // @lc code=start
-pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
+pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> Option<i32> {
     // Gas Station
     let n = match gas.len() == cost.len() {
         true => gas.len(),
         false => panic!("lengths don't match"),
     };
-    (0..n)
-        .filter_map(|i| {
-            let indices_iterator = (0..n).map(|j| (j+i) % n);
-            let mut gas_current = 0;
-            for j in indices_iterator{
-                gas_current += gas[j];
-                if gas_current < cost[j]{
-                    return None;
-                }else{
-                    gas_current -= cost[j]
-                }
-            };
-            Some(i)
-        })
-        .next()
-        .map(|x| x as i32)
-        .unwrap_or(-1)
+    let gas_minus_cost_vec = gas
+        .iter()
+        .zip(cost.iter())
+        .map(|(g, c)| g - c)
+        .collect::<Vec<_>>();
+    match gas_minus_cost_vec.iter().sum::<i32>() < 0 {
+        true => None,
+        false => {
+            // let change_indices = gas_minus_cost_vec.iter().enumerate().scan(
+            //     std::cmp::Ordering::Less,
+            //     |ordering, (i, x)| {
+            //         let new_ordering = x.cmp(&0);
+            //         match &new_ordering == ordering {
+            //             true => Some(None),
+            //             false => {
+            //                 *ordering = new_ordering.clone();
+            //                 Some(Some((i, new_ordering)))
+            //             }
+            //         }
+            //     },
+            // );
+            let index = gas_minus_cost_vec
+                .iter()
+                .scan(0, |acc, &x| {
+                    *acc += x;
+                    Some(*acc)
+                })
+                .enumerate()
+                .min_by_key(|(_, k)| *k)
+                .map(|(i, _)| i)?;
+            Some(((index + 1) % n) as i32)
+        }
+    }
 }
 
 impl Solution {
     pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
-        can_complete_circuit(gas, cost)        
+        can_complete_circuit(gas, cost).unwrap_or(-1)
     }
 }
 // @lc code=end
-
