892162312a9bd202ed61ddafdab3d10a4808c229
azhar0100 <azhartahir0100@gmail.com>
2025-03-20 03:25:31 +0500

    Added code

--- rust/49.group-anagrams.rs
+++ rust/49.group-anagrams.rs
@@ -8,25 +8,23 @@
 use std::collections::HashMap;
 
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    let mut sorted_strs = strs
-        .iter()
+    strs.iter()
         .map(|x| {
             let mut char_vec = x.chars().collect::<Vec<_>>();
             char_vec.sort();
             char_vec.into_iter().collect::<String>()
         })
         .enumerate()
-        .collect::<Vec<_>>();
-    let fold_map = sorted_strs.iter().fold(HashMap::new(), |mut acc, &(i, ref x)| {
-        let entries = acc.entry(x).or_insert(Vec::new());
-        entries.push(i);
-        acc
-    });
-    fold_map
+        .fold(HashMap::new(), |mut acc, (i, x)| {
+            let entries = acc.entry(x).or_insert(Vec::new());
+            entries.push(i);
+            acc
+        })
         .into_iter()
         .map(|(_, v)| v.into_iter().map(|i| strs[i].clone()).collect::<Vec<_>>())
         .collect()
 }
+
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
--- rust/leetcode-rust/src/problems/p0049_group_anagrams.rs
+++ rust/leetcode-rust/src/problems/p0049_group_anagrams.rs
@@ -1,22 +1,18 @@
 use std::collections::HashMap;
 
 pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    let mut sorted_strs = strs
-        .iter()
+    strs.iter()
         .map(|x| {
             let mut char_vec = x.chars().collect::<Vec<_>>();
             char_vec.sort();
             char_vec.into_iter().collect::<String>()
         })
         .enumerate()
-        .collect::<Vec<_>>();
-    sorted_strs.sort_by_key(|(_, x)| x.clone());
-    let fold_map = sorted_strs.iter().fold(HashMap::new(), |mut acc, &(i, ref x)| {
-        let entries = acc.entry(x).or_insert(Vec::new());
-        entries.push(i);
-        acc
-    });
-    fold_map
+        .fold(HashMap::new(), |mut acc, (i, x)| {
+            let entries = acc.entry(x).or_insert(Vec::new());
+            entries.push(i);
+            acc
+        })
         .into_iter()
         .map(|(_, v)| v.into_iter().map(|i| strs[i].clone()).collect::<Vec<_>>())
         .collect()
