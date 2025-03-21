98cdeb29ed9e0e49a5c67fd0533392307b90edda
azhar0100 <azhartahir0100@gmail.com>
2025-03-22 00:33:56 +0500

    Added code

--- rust/leetcode-rust/src/main.rs
+++ rust/leetcode-rust/src/main.rs
@@ -72,10 +72,10 @@ fn main() {
     // let res = jump(vec![2,1]);
     // println!("{:?}",res);
     // let res = can_complete_circuit(vec![2,3,4], vec![3,4,3]);
-    // let mut input_mat = vec![vec![1,2,3],vec![4,5,6],vec![7,8,9]];
-    // println!("{:?}",input_mat);
-    // let res = p0048_rotate_image::rotate(&mut input_mat);
-    // println!("{:?}",input_mat);
+    let mut input_mat = vec![vec![1,2,3,4],vec![5,6,7,8],vec![9,10,11,12],vec![13,14,15,16]];
+    println!("{:?}",input_mat);
+    let res = p0048_rotate_image::rotate(&mut input_mat);
+    println!("{:?}",input_mat);
     // let input_mat = vec![
     //     vec!['5','3','.','.','7','.','.','.','.'],
     //     vec!['6','.','.','1','9','5','.','.','.'],
@@ -87,7 +87,7 @@ fn main() {
     //     vec!['.','.','.','4','1','9','.','.','5'],
     //     vec!['.','.','.','.','8','.','.','7','9']];
     // let res = problems::p0036_valid_sudoku::is_valid_sudoku(input_mat);
-    let mut input_mat = vec![vec![0,1,2,0],vec![3,4,5,2],vec![1,3,1,5]];
-    problems::p0073_set_matrix_zeroes::set_zeroes(&mut input_mat);
+    // let mut input_mat = vec![vec![0,1,2,0],vec![3,4,5,2],vec![1,3,1,5]];
+    // problems::p0073_set_matrix_zeroes::set_zeroes(&mut input_mat);
     // println!("{:?}",res);
 }
--- rust/leetcode-rust/src/problems/p0048_rotate_image.rs
+++ rust/leetcode-rust/src/problems/p0048_rotate_image.rs
@@ -1,32 +1,82 @@
 use std::collections::HashMap;
+// pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+//     let len = nums.len();
+//     let effective_k = (k % (len as i32)) as usize;
+//     let mut k_sized_vec = Vec::with_capacity(effective_k);
+//     for o in (0..len).rev() {
+//         let n = o + effective_k;
+//         match n < len {
+//             true => nums[n] = nums[o],
+//             false => k_sized_vec.push(nums[o]),
+//         }
+//     }
+//     for (i, val) in k_sized_vec.into_iter().rev().enumerate() {
+//         nums[i] = val
+//     }
+// }
+
+// pub fn rotate_constant_extra_space(nums: &mut Vec<i32>, constant_factor: i32, k: i32) {
+//     let number_of_complete_constant_factor_rotations = k / constant_factor;
+//     let last_rotation = k % constant_factor;
+//     let rotation_number = (0..number_of_complete_constant_factor_rotations)
+//         .map(|_| constant_factor)
+//         .chain(std::iter::once(last_rotation));
+//     for k in rotation_number{
+//         rotate(nums, k);
+//     }
+// }
+
+pub fn rotate_given_idxs_of_square_matrix<T>(
+    matrix: &mut Vec<Vec<T>>,
+    idxes: &[(usize, usize)],
+    k: usize,
+) where
+    T: Copy,
+{
+    println!("rotating k {:?}, idxes: {:?}", k, idxes);
+    let len = idxes.len();
+    let effective_k = k % len;
+    let mut k_sized_vec = Vec::with_capacity(effective_k);
+    for o in (0..len).rev() {
+        let n = o + effective_k;
+        match n < len {
+            true => matrix[idxes[n].0][idxes[n].1] = matrix[idxes[o].0][idxes[o].1],
+            false => k_sized_vec.push(matrix[idxes[o].0][idxes[o].1]),
+        }
+    }
+    for (i, val) in k_sized_vec.into_iter().rev().enumerate() {
+        matrix[idxes[i].0][idxes[i].1] = val
+    }
+}
+
+pub fn rotate_square_boundary(matrix: &mut Vec<Vec<i32>>, boundary_idx: usize) {
+    let matrix_n = matrix.len();
+    let matrix_boundary_length = matrix_n - (boundary_idx * 2);
+    let n = matrix_boundary_length;
+    let boundary_idxes = (0..4 * n)
+        .filter_map(|i| {
+            let div = i / n;
+            let rem = i % n;
+            match (div, rem) {
+                (0, _) => Some((0, rem)),
+                (1, 0) => None,
+                (1, _) => Some((rem, n - 1)),
+                (2, 0) => None,
+                (2, _) => Some((n - 1, n - 1 - rem)),
+                (3, 0) => None,
+                (3, val) if val == n - 1 => None,
+                (3, _) => Some((n - 1 - rem, 0)),
+                _ => panic!("Invalid div: {}", div),
+            }
+        })
+        .map(|(i, j)| (i + boundary_idx, j + boundary_idx));
+    rotate_given_idxs_of_square_matrix(matrix, &boundary_idxes.collect::<Vec<_>>(), n - 1);
+}
 
 pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
-    let n = matrix.len();
-    let indices_iter = (0..n).flat_map(|i| {
-        let i_clone = i.clone();
-        (0..n).map(move |j| (i_clone, j))
-    }).map(|(i,j)| {
-        ((i,j),(j,n-1-i))
-    });
-    let mut index_history = HashMap::new();
-    for (old, new) in indices_iter{
-        
-        let (i_old,j_old) = index_history.get(&old).unwrap_or(&old).clone();
-        let (i_new,j_new) = index_history.get(&new).unwrap_or(&new).clone();
-        println!("{:?} -> {:?}",(i_old,j_old), (i_new,j_new));
-        // let a = matrix[i_old][j_old]
-        // let b = matrix[i_new][j_new]
-        // let a = b ^ a``
-        // let b = a ^ b
-        // let a = b ^ a
-        (matrix[i_old][j_old],matrix[i_new][j_new]) = (matrix[i_new][j_new],matrix[i_old][j_old]);
-        index_history.insert(old, new);
-        // let 
-    };
-    // for i in (0..n){
-    //     let row = matrix[i].as_mut_slice();
-    //     for j in 0..(n/2) {
-    //         (row[j],row[n-1-j]) = (row[n-1-j],row[j])
-    //     }
-    // }
+    let matrix_n = matrix.len();
+    let boundary_idxes = (0..matrix_n / 2).map(|i| i);
+    for boundary_idx in boundary_idxes {
+        rotate_square_boundary(matrix, boundary_idx);
+    }
 }
