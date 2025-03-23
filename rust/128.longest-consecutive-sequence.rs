b29d0516566e1ac3d86a497f63e2e12e8ba20800
azhar0100 <azhartahir0100@gmail.com>
2025-03-24 02:58:41 +0500

    Added new code

--- rust/128.longest-consecutive-sequence.rs
+++ rust/128.longest-consecutive-sequence.rs
@@ -5,10 +5,57 @@
  */
 
 // @lc code=start
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ScanState {
+    pub visited: HashSet<i32>,
+    pub unvisited: HashSet<i32>,
+    pub longest: i32,
+}
+
+pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+    let nums_set: HashSet<i32> = nums.into_iter().collect();
+    nums_set
+        .iter()
+        .fold(
+            ScanState {
+                visited: HashSet::new(),
+                unvisited: nums_set.clone(),
+                longest: 0,
+            },
+            |mut state, &num| match state.visited.contains(&num) {
+                true => state,
+                false => {
+                    let mut current = num;
+                    let mut current_longest = 1;
+                    while state.unvisited.contains(&(current + 1)) {
+                        current += 1;
+                        current_longest += 1;
+                        state.visited.insert(current);
+                        state.unvisited.remove(&current);
+                    }
+                    current = num;
+                    while state.unvisited.contains(&(current - 1)) {
+                        current -= 1;
+                        current_longest += 1;
+                        state.visited.insert(current);
+                        state.unvisited.remove(&current);
+                    }
+                    state.longest = state.longest.max(current_longest);
+                    state.visited.insert(num);
+                    state.unvisited.remove(&num);
+                    state
+                }
+            },
+        )
+        .longest
+}
+
+
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
-        
+        longest_consecutive(nums)
     }
 }
 // @lc code=end
-
